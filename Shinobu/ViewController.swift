e659c5beaf4adf074899bc6d5efacd10acc53224
Nyx0uf <benjgodard@gmail.com>
2018-12-20 21:51:01 +0100

    init

---
+++ Shinobu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shinobu
+//
+//  Created by Benjamin on 20/12/2018.
+//  Copyright © 2018 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
