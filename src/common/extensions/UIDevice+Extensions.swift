57f0a2eba2391ee22e6c483f349bb3ce5ae5ef7f
Nyx0uf <benjgodard@whine.fr>
2020-11-15 11:35:50 +0100

    *iPhone 12

--- src/common/extensions/UIDevice+Extensions.swift
+++ src/common/extensions/UIDevice+Extensions.swift
@@ -12,7 +12,12 @@ extension UIDevice {
 	func isPhoneX() -> Bool {
 		if isPhone() {
 			let height = Int(UIScreen.main.nativeBounds.height)
-			return height == 2436 || height == 2688 || height == 1792
+			return height == 2436 /* X | Xs | 11 Pro */
+				|| height == 2688 /* Xs Max | 11 Pro Max */
+				|| height == 1792 /* Xr | 11 */
+				|| height == 2532 /* 12 | 12 Pro */
+				|| height == 2340 /* 12 Mini */
+				|| height == 2778 /* 12 Pro Max */
 		}
 		return false
 	}
